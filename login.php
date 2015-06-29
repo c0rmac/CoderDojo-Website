c908d2b4ae37c9bb7212107606aed9264f902498
c0rmac <mastercormackinsella@gmail.com>
2015-06-29 20:05:42 +0100

    Added register.html and set side bar links
    -Added register.html to src with fully designed form
    -Changed side bar links for login.html and register.html in the files
    index.html, login.html and register.html

--- login.php
+++ login.php
@@ -12,7 +12,10 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 	    while($row = $result->fetch_assoc()) {
 	if($_POST["email"] == $row["email"]){
-		echo "Indeed you got the submited information right";
+		echo "Oi you, ya got the email right, lets see 'bout the password now ya junkie";
+		if($_POST["password"] == 'password'){
+			echo "<p>guess what ya junkie, the flippin password is roight, now ya can go smuggle weed 'cross the border</p>"
+		}
 	}
 	else{
 		echo "Data is not in server, did you enter the right info?";
