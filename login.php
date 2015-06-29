5dfe5afd7bf1d9194987a323cbd623953624d69a
c0rmac <mastercormackinsella@gmail.com>
2015-06-29 19:02:42 +0100

    Now you can login (password doesnt work)
    -Connects to database and retrives email
    -Changed login.html to 127.0.0.1/login.php as a server address rather
    than "login.php" which is a local address

--- login.php
+++ login.php
@@ -2,10 +2,26 @@
 
 if (isset($_POST["email"]) && ($_POST["password"])) 
 {
-	echo "login will initiate";
+	$email = $_POST["email"];
+	include_once "config.php";
+	
+$sql = "SELECT email, password_hash FROM users";
+$result = $conn->query($sql);
+
+//Login initaites
+if ($result->num_rows > 0) {
+	    while($row = $result->fetch_assoc()) {
+	if($_POST["email"] == $row["email"]){
+		echo "Indeed you got the submited information right";
+	}
+	else{
+		echo "Data is not in server, did you enter the right info?";
+	}
+		} 
+}
 }
 else {
-	echo "no info found";
+	echo "You left the password field blank";
 }
 
 ?>
